c0ef266db5836a59dec82d8a06c0603e482fc899
nextrevision <notarobot@nextrevision.net>
2024-04-22 10:06:46 -0700

    Add negative tests

--- store/clickhousestore/reader_test.go
+++ store/clickhousestore/reader_test.go
@@ -20,3 +20,31 @@ func TestClickhouseReader_padTraceIDs(t *testing.T) {
 	res := cr.padTraceIDs([]string{"c91fd0eb7e1193f8", "c91fd0eb7e1193f9"})
 	assert.Equal(t, []string{"0000000000000000c91fd0eb7e1193f8", "0000000000000000c91fd0eb7e1193f9"}, res)
 }
+
+func TestClickhouseReader_padTraceIDs_32(t *testing.T) {
+	db, _, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	tracer := trace.NewNoopTracerProvider().Tracer("test-tracer")
+
+	cr := New("test", true, db, tracer)
+	res := cr.padTraceIDs([]string{"0000000000000000c91fd0eb7e1193f8", "0000000000000000c91fd0eb7e1193f9"})
+	assert.Equal(t, []string{"0000000000000000c91fd0eb7e1193f8", "0000000000000000c91fd0eb7e1193f9"}, res)
+}
+
+func TestClickhouseReader_padTraceIDs_skiplen(t *testing.T) {
+	db, _, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	tracer := trace.NewNoopTracerProvider().Tracer("test-tracer")
+
+	cr := New("test", true, db, tracer)
+	res := cr.padTraceIDs([]string{"0c91fd0eb7e1193f8", "0c91fd0eb7e1193f9"})
+	assert.Equal(t, []string{"0c91fd0eb7e1193f8", "0c91fd0eb7e1193f9"}, res)
+}
